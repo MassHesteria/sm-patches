88378e318f92e3c4055bc488e3e136a138ea9cca
MassHesteria <brad.hester@gmail.com>
2021-01-03 13:55:42 -0600

    Initial commit

---
+++ include/snes.asm
@@ -0,0 +1,22 @@
+architecture wdc65816
+
+macro seek(variable offset) {
+  origin ((offset & $7F0000) >> 1) | (offset & $7FFF)
+  base offset
+}
+
+macro check(define name, variable addr) {
+   if pc() != addr {
+      print {name}, " INCORRECT!\n"
+      print "  is: ",hex:pc(),"\n"
+      print "  should be: ",hex:addr,"\n"
+   }
+}
+
+macro insert_nop(define num) {
+   define i = {num}
+   while {i} > 0 {
+      nop
+      evaluate i = {i} - 1
+   }
+}
